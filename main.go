f963fa346487d63a8985661957b0dd6acc26d968
italotorres <italotorres3@gmail.com>
2025-03-06 16:33:29 -0300

    adcionando rascunho para as aulas de go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello " + " World")
+	fmt.Println("2 + 4 =", 2+4)
+	fmt.Println("10.0 / 2.0 =", 10.0/2.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
